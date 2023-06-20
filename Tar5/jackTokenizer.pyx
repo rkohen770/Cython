2a714c65901daa9d3b6b691192890c209ff48d41
rachel lea kohen <rkohen@g.jct.ac.il>
2023-06-20 18:23:51 +0300

    start Tar 5

---
+++ Tar5/jackTokenizer.pyx
@@ -0,0 +1,139 @@
+# class that Ignores all comments and white space, gets the next token,
+# and advances the input just beyond it.
+
+
+import os
+from jackAnalyzer import JackAnalyzer
+
+cdef class JackTokenizer:
+
+    # Constants
+    KEYWORDS = ["class", "constructor", "function", "method", "field", "static", "var", "int", "char", "boolean",
+                "void", "true", "false", "null", "this", "let", "do", "if", "else", "while", "return"]
+    SYMBOLS = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
+    KEYWORD = "KEYWORD"
+    SYMBOL = "SYMBOL"
+    IDENTIFIER = "IDENTIFIER"
+    INT_CONST = "INT_CONST"
+    STRING_CONST = "STRING_CONST"
+
+    cdef str input_file_path
+    cdef str current_token
+    cdef str current_token_type
+
+    # recursive function to process each file in the folder
+    cpdef recursive_process(self, file_path):
+        for entry in os.scandir(file_path):
+            print(entry.path)
+
+            if entry.is_file() and entry.name.endswith('.jack'):
+            
+                # Extract the name of the file in the path without the extension
+                file_name = os.path.splitext(entry.name)[0]
+                print(file_name)
+
+                 # Build the output file name
+                output_file_name = os.path.join(file_path, file_name + 'T' + '.xml')
+        
+                # Open the output file for writing
+                output_file = open(output_file_name, 'w')
+                
+                self.process_file(entry.path, output_file)
+                output_file.close()
+
+                # Create an instance of the JackAnalyzer class
+                analyzer = JackAnalyzer()
+
+                # Call the compile method on the analyzer instance, passing the output file and directory as arguments
+                analyzer.compile(output_file_name, file_path , file_name)
+
+            elif entry.is_dir():
+                self.recursive_process(entry.path)
+
+
+
+    # process each file
+    cdef process_file(self, input_file, output_file):
+
+        output_file.write("<tokens>\n")
+        # open the input file
+        with open(input_file, 'r') as f:
+            # open the output file
+            for line in f:
+                # remove white spaces and tabs in the beginning and end of the line
+                line = line.strip()
+
+                # ignore comments and empty lines
+                if line.startswith("//") or line.startswith("/**") or line.startswith("\n") or line.startswith("*"):
+                    continue
+                # remove comments
+                elif "//" in line:
+                    line = line[:line.index("//")]
+                elif "/*" in line:
+                    line = line[:line.index("/*")]
+                    while "*/" not in line:
+                        line += next(f)
+                    line = line[line.index("*/"):]
+
+                # remove empty lines
+                if line.strip() == "":
+                    continue
+                # remove white spaces and tabs in the beginning and end of the line
+                line = line.strip()
+                # add the line to the output file
+                self.processLine(line, output_file)
+
+
+                
+
+            output_file.write("</tokens>\n")
+
+
+    # process each line
+    cdef processLine(self, line, output_file):
+        token = ''
+        flag = False
+        for sign in line:
+            if sign in self.SYMBOLS and not flag:
+                self.tipulInWord(token, output_file)
+                if sign =='<' and not flag:
+                    output_file.write("<symbol> &lt; </symbol>\n")
+                elif sign =='>' and not flag:
+                    output_file.write("<symbol> &gt; </symbol>\n")
+                elif sign =='&' and not flag:
+                    output_file.write("<symbol> &amp; </symbol>\n")
+                else:
+                    output_file.write("<symbol> " + sign + " </symbol>\n")
+                token = ""
+            elif sign == ' ' and not flag:
+                self.tipulInWord(token, output_file)
+                token = ""
+            elif sign == '/t' and not flag:
+                self.tipulInWord(token, output_file)
+                token = ""
+            elif sign == '"':
+                if flag:
+                    output_file.write("<stringConstant> " + token +" </stringConstant>\n")
+                    flag = False
+                    token = ""
+                else:
+                    flag = True
+            else:
+                token += sign
+
+
+
+    # check the type of the word
+    cdef tipulInWord(self, word, output_file):
+        if word==" " or word=="\t" or word=="" or word.strip()=="":
+            return
+        if word in self.KEYWORDS:
+            output_file.write("<keyword> " + word + " </keyword>\n")
+        elif word.isdigit():
+            output_file.write("<integerConstant> " + word + " </integerConstant>\n")
+        elif word.startswith('"') and word.endswith('"'):
+            output_file.write("<stringConstant> " + word + " </stringConstant>\n")
+        else:
+            output_file.write("<identifier> " + word + " </identifier>\n")
+
+
---
+++ Tar5/main.py
@@ -0,0 +1,15 @@
+# קבוצה: 150060.31.5783.01, 150060.31.5783.43
+# רחל לאה כהן: 211372156
+# הודיה שנקר: 211720503
+
+# This code is always interpreted, like normal Python.
+# It is not compiled to C.
+
+import hello
+import jackTokenizer
+
+hello.say_hello()
+
+jt = jackTokenizer.JackTokenizer()
+jt.recursive_process("C:\\Users\\rache\\PycharmProjects\\Cython\\Tar5\\11")
+# jt.recursive_process("C:\\Users\\hoday\\source\\repos\\Cython\\Cython\\Tar5\\11")
